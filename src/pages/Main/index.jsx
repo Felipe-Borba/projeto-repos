3abf8d63c1f4cc3e6ae08f3d570e601cce2c9f16
Felipe-Borba <felipe.borba0109@gmail.com>
2022-03-21 21:52:34 -0300

    fetch repo from github

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { FaGithub, FaPlus } from "react-icons/fa";
+import api from "../../services/api";
 import { Container, Form, SubmitButton } from "./styles";
 
 function Main() {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositorios, setRepositorios] = useState([]);
+
+  function handleInputChange(e) {
+    setNewRepo(e.target.value);
+  }
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      async function submit() {
+        const response = await api.get(`repos/${newRepo}`);
+
+        const data = {
+          name: response.data.full_name,
+        };
+
+        setRepositorios((old) => [...old, ...data]);
+        setNewRepo("");
+      }
+
+      submit();
+    },
+    [newRepo]
+  );
+
   return (
     <Container>
       <h1>
@@ -10,8 +38,13 @@ function Main() {
         Meus Repositórios
       </h1>
 
-      <Form>
-        <input type="text" placeholder="Adicionar Repositórios" />
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Adicionar Repositórios"
+          value={newRepo}
+          onChange={handleInputChange}
+        />
 
         <SubmitButton>
           <FaPlus color="#fff" size={24} />
