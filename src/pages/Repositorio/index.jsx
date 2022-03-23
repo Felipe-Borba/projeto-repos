748526f4f33d687819ac04aafa0791f0deb50a5f
Felipe-Borba <felipe.borba0109@gmail.com>
2022-03-22 22:19:45 -0300

    fetch repo info from api

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import api from "../../services/api";
+import { Container } from "./styles";
 
 function Repositorio() {
   const params = useParams();
 
+  const [repositorio, setRepositorio] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function load() {
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${params.repositorio}`),
+        api.get(`/repos/${params.repositorio}/issues`, {
+          params: {
+            state: "open",
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      setRepositorio(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    load();
+  }, [params]);
+
   return (
-    <div>
-      <h1 style={{ color: "#fff" }}>{params.repositorio}</h1>
-    </div>
+    <Container>
+      <h1>{params.repositorio}</h1>
+    </Container>
   );
 }
 
